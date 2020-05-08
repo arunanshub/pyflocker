43afd1d59e61da5d116c211c02554ffb1c3011fd
Arunanshu Biswas <mydellpc07@gmail.com>
2020-05-08 15:34:14 +0530

    cleanse

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyFLocker',
+
+    version='0.2.1',
+    author="Arunanshu Biswas",
+    author_email="mydellpc07@gmail.com",
+
+    packages=find_packages(),
+    license='MIT License',
+
+    description="File Locking library",
+    long_description=open("README.md").read(),
+
+    url="https://github.com/arunanshub/pyflocker",
+)
+
