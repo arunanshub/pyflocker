5d63e5e1e1f101015b182a20d93603f46480a18a
Arunanshu Biswas <mydellpc07@gmail.com>
2020-05-12 22:22:30 +0530

    backend reorganization; import procedure changed

--- pyflocker/ciphers/__init__.py
+++ pyflocker/ciphers/__init__.py
@@ -1,13 +1,38 @@
 """The `ciphers` package
 This package provides:
+
     - wrappers for cryptographic primitives to add support for
       encrypting/decrypting files and file streams
     - base classes to implement new wrappers
       (available in `pyflocker.ciphers.base`)
-All cipher implementations are stored here in their own modules.
+
+
+The Design Principle
+~~~~~~~~~~~~~~~~~~~~
+
+This package was designed with the prime idea that the ciphers' interfaces
+and their backend specific implementation counterpart must be kept separate,
+hence this package is divided into two main parts:
+
+    - `ciphers.interfaces` : to provide the frontend to the implemented 
+                             ciphers.
+    - `ciphers.backends` : to provide the implementation counterparts of
+                           ciphers.
+
+
+The Backend's Organization
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+The backend has been designed in such a way that each cipher is kept separate
+in its own module, and each backend has its own separate package.
+For example, the AES cipher implemented with Crypto(dome) is kept in
+`backends.cryptodome_.AES`.
+
 Read their documentation to learn more.
 """
 
 
-from .backends import load_backend, Backends
+from .backends import load_cipher, Backends
 
+# load interfaces
+from .interfaces import AES
--- pyflocker/ciphers/backends/__init__.py
+++ pyflocker/ciphers/backends/__init__.py
@@ -3,7 +3,7 @@
 from enum import Enum
 from importlib import import_module
 
-from .. import base, exc
+from .. import base, exc, interfaces
 
 
 class Backends(Enum):
@@ -15,8 +15,29 @@ class Backends(Enum):
     CRYPTO = CRYPTODOME
 
 
+
+def load_interface(name):
+    """Loads the interface module defined in `..interfaces`.
+    This is used by the cipher-backends to load its interface counterpart.
+    """
+    return import_module(f".{name}",
+        interfaces.__package__)
+
+
+def load_cipher(name, bknd=None):
+    """Loads the cipher module from backend.
+    Generally used by interfaces to load the implemented cipher counterpart.
+    """
+    bknd = load_backend(bknd)
+    try:
+        return import_module(f".{name}",
+            bknd.__package__)
+    except ModuleNotFoundError:
+        raise NotImplementedError(f"cipher {name} unsupported")
+
+
 def load_backend(bknd=None):
-    """Loads backend.
+    """Loads backend package.
     Backend must be an attribute of `Backends`.
     If `None` is supplied, loads whichever backenf is available.
     If both are available, loads `Cryptodome`.
