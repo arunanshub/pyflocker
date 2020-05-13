6c5ea1421675c66459ae4c2d9d5227e12e75d977
Arunanshu Biswas <mydellpc07@gmail.com>
2020-05-13 16:09:21 +0530

    added Camellia cipher

---
+++ pyflocker/ciphers/backends/cryptography_/Camellia.py
@@ -0,0 +1,31 @@
+from cryptography.hazmat.primitives.ciphers import (Cipher,
+        algorithms as algo, modes)
+from cryptography.hazmat.backends import default_backend as defb
+import hmac
+
+from .. import base, exc, load_interface as _if
+from .AES import NonAEAD as _NonAEAD
+
+
+_m = _if("Camellia")
+
+supported = {
+    _m.MODE_CFB : modes.CFB,
+    _m.MODE_CTR : modes.CTR,
+    _m.MODE_OFB : modes.OFB,
+}
+
+
+class NonAEAD(_NonAEAD):
+
+    def __init__(self, file, locking, key, mode, *args, **kwargs):
+
+        digestmod = kwargs.pop('digestmod', 'sha256')
+        self._file = file
+        self._locking = locking
+
+        _cipher = Cipher(algo.Camellia(key), supported[mode](*args, **kwargs), defb())
+        self._cipher = _cipher.encryptor() if locking else _cipher.decryptor()
+
+        self._hasher = hmac.new(key, digestmod=digestmod)
+
---
+++ pyflocker/ciphers/interfaces/Camellia.py
@@ -0,0 +1,27 @@
+"""Interface to Camellia cipher"""
+
+
+from .. import load_cipher as _load_cpr
+from .. import Backends
+
+
+def _cml_cipher_from_mode(mode, bknd):
+    if mode not in bknd.supported.keys():
+        raise NotImplementedError(
+            "backend does not support this mode.")
+    return bknd.NonAEAD 
+
+
+def new(file, locking, key, mode, *args, backend=Backends.CRYPTOGRAPHY, **kwargs):
+    cpr = _load_cpr("Camellia", backend)
+    return _cml_cipher_from_mode(mode, cpr)(
+        file, locking, key, mode, *args, **kwargs)
+
+
+# supported modes
+MODE_CFB = "MODE_CFB"
+
+MODE_CTR = "MODE_CTR"
+
+MODE_OFB = "MODE_OFB"
+
