af1166cb13d2ca0c1bd07b0ddbbd95b37ee8efd5
Arunanshu Biswas <mydellpc07@gmail.com>
2020-05-15 14:30:48 +0530

    added documentation to ciphers

--- pyflocker/ciphers/__init__.py
+++ pyflocker/ciphers/__init__.py
@@ -1,34 +1,21 @@
 """The `ciphers` package
-This package provides:
 
-    - wrappers for cryptographic primitives to add support for
-      encrypting/decrypting files and file streams
-    - base classes to implement new wrappers
-      (available in `pyflocker.ciphers.base`)
-
-
-The Design Principle
+Parts of the package
 ~~~~~~~~~~~~~~~~~~~~
+    
+- interfaces : Provides seamless interface to various ciphers
+               implemented (and supported) by backend.
 
-This package was designed with the prime idea that the ciphers' interfaces
-and their backend specific implementation counterpart must be kept separate,
-hence this package is divided into two main parts:
+- modes : Contains the modes that are supported by the
+          ciphers (in `Modes` class). Additionally, the
+          modes are categorized according to their type.
 
-    - `ciphers.interfaces` : to provide the frontend to the implemented 
-                             ciphers.
-    - `ciphers.backends` : to provide the implementation counterparts of
-                           ciphers.
+- backends : Provides the implementation counterpart of ciphers.
+             Each backend has its own package.
 
+- base : Base class and utility tools to wrap new ciphers.
 
-The Backend's Organization
-~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-The backend has been designed in such a way that each cipher is kept separate
-in its own module, and each backend has its own separate package.
-For example, the AES cipher implemented with Crypto(dome) is kept in
-`backends.cryptodome_.AES`.
-
-Read their documentation to learn more.
+- exc : Exceptions raised by `pyflocker`.
 """
 
 
