663700817d92dfad66dba7466e8a0a9e3fe2e3c2
Arunanshu Biswas <mydellpc07@gmail.com>
2020-05-16 14:32:52 +0530

    Debug to base.py
    Previously, cipher changed class after finalization,
    which made debugging difficult and more verbose, and
    the inheritance errors were frequent.
    
    Now, the ciphers simply inherit from Cipher (no BaseCipher etc.)
    and the methods are decorated as usual.

--- pyflocker/ciphers/base.py
+++ pyflocker/ciphers/base.py
@@ -6,12 +6,12 @@
 import sys
 
 from functools import wraps
-
 from abc import ABC, abstractmethod
+
 from . import exc
 
 
-class BaseCipher(ABC):
+class Cipher(ABC):
     """Base cipher for all other ciphers."""
     
     @abstractmethod
@@ -69,112 +69,39 @@ class BaseCipher(ABC):
         """
 
 
-class Cipher(BaseCipher):
-    """Base class for non-finalized (usable) ciphers."""
-
-    def calculate_tag(self):
-        raise exc.NotFinalized("Ciphers must be finalized before calculating tag.")
-
-
-class ClosedCipher(BaseCipher):
-    """Base class for finalized ciphers."""
-
-    def update(self, *args, **kwargs):
-        raise exc.AlrealyFinalized("cannot write to an already finalized cipher.")
-
-    def update_into(self, *args, **kwargs):
-        raise exc.AlrealyFinalized("cannot write to an already finalized cipher.")
-
-    def authenticate(self, *args, **kwargs):
-        raise exc.AlrealyFinalized("cannot authenticate any more data")
-
-    def finalize(self, *args, **kwargs):
-        raise exc.AlrealyFinalized("cipher is already finalized.")
-
-
 # ===========================================================
 # Decorators and utils for simplifying the creation of cipher wrappers.
 
 
-def _new_state_class(from_cls, to_cls, name,
-                     modname=None):
-    """Creates new state class with required bases.
-
-    Avoids the bug in __init_subclass__ when using
-    types.new_class
-    """
-    klass = from_cls
-
-    # simply insert the class into base class tuple
-    # side effect: the new class remains a from_cls instance,
-    # even if ClosedCipher is not a subclass of Cipher
-    bases = set(klass.__bases__ + (to_cls, ))
-
-    # update class dict
-    clsdict = dict(from_cls.__dict__)
-    clsdict.update(dict(to_cls.__dict__))
-
-    # make the class directly
-    new = type(name, tuple(bases), clsdict)
-
-    new.__module__ = modname or __name__
-    return new
-
-
-def set_state(cls):
-    """Decorator to change cipher's state.
-    
-    Used this decorator to change the state of the cipher.
-    It must be applied on the method that is called during finalization.
-    It can be called only once.
-    """
-
-    def decorator(f):
-        @wraps(f)
-        def wrapper(self, *args, **kwargs):
-            if isinstance(self, cls):
-                raise exc.AlrealyFinalized(
-                    "this method can be called only once "
-                    "during finalization")
-            try:
-                return f(self, *args, **kwargs)
-            finally:
-                if sys.exc_info()[0] in (None, exc.DecryptionError):
-                    cal = self.calculate_tag
-
-                    # make new state class and replace
-                    new = _new_state_class(
-                        self.__class__,
-                        cls,
-                        self.__class__.__name__,
-                        self.__module__)
-                    self.__class__ = new
-
-                    # If `calculate_tag` is decorated, 
-                    # replace with the decorated version.
-                    if hasattr(cal, "__wrapped__"):
-                        self.calculate_tag = cal
-        return wrapper
-    return decorator
-
-
 def finalizer(f):
-    """Applied on the method that ends the cipher's context.
-    It changes the cipher to `ClosedCipher` instance.
-    """
-    return set_state(ClosedCipher)(f)
-
-
-def before_finalized(f):
-    """Use this decorator to set the method to be called only if
-    the cipher is not finalized yet.
-
-    This is generally required if you are making the cipher manually,
-    and need to control it's pre-finalized state's behaviour.
+    """Finalizes the cipher.
+    The wrapped function must be called only once in the
+    entire cipher context.
     """
     @wraps(f)
     def wrapper(self, *args, **kwargs):
-        if not isinstance(self, ClosedCipher):
+        if hasattr(self, '_done_'):
+            raise exc.AlrealyFinalized("cipher has already been finalized")
+
+        try:
+            return f(self, *args, **kwargs)
+        finally:
+            if sys.exc_info()[0] in (
+                exc.DecryptionError, None):
+                self.__class__._done_ = property(
+                    lambda self: True)
+    return wrapper
+
+
+def before_finalized(f):
+    """Methods decorated with this decorator can only be called
+    before the cipher has been finalized.
+
+    see: `finalizer`
+    """
+    @wraps(f)
+    def wrapper(self, *args, **kwargs):
+        if not hasattr(self, '_done_'):
             return f(self, *args, **kwargs)
         raise exc.AlrealyFinalized(
             "this method can only be called before finalizing")
@@ -182,17 +109,18 @@ def before_finalized(f):
 
 
 def after_finalized(f):
-    """Use this decorator to set the method to be called
-    only after `finalize` has been called.
+    """Methods decorated with this decorator can only be called
+    after the cipher has been finalized.
+
+    see: `finalizer`
     """
     @wraps(f)
     def wrapper(self, *args, **kwargs):
-        if isinstance(self, ClosedCipher):
+        if hasattr(self, '_done_'):
             return f(self, *args, **kwargs)
         raise exc.NotFinalized(
             "Ciphers must be finalized before calling this method.")
     return wrapper
-
 
 
 # ===============================================
@@ -200,22 +128,21 @@ def after_finalized(f):
 
 
 def cipher(cls):
-    """Decorator to create a Cipher class.
+    """Decorator to create a Cipher wrapper.
     It must be applied on a class that derives from
-    `OpenedCipher` class.
+    `Cipher` class.
     """
     if not issubclass(cls, Cipher):
         raise TypeError(
         f"Class must be derived from `{Cipher.__name__}`")
 
-    # check for explicitly defined calculate_tag method
-    if Cipher.calculate_tag == cls.calculate_tag:
-        raise TypeError("class must explicitly define `calculate_tag` "
-                "method to use this decorator")
-
     # decorate methods
     cls.finalize = finalizer(cls.finalize)
     cls.calculate_tag = after_finalized(
-            cls.calculate_tag)
+        cls.calculate_tag)
+    cls.authenticate = before_finalized(cls.authenticate)
+    cls.update = before_finalized(cls.update)
+    cls.update_into = before_finalized(cls.update_into)
+
     return cls
 
