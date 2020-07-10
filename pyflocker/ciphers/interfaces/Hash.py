ba8f75bf50dcf49405019d5bd40b9f3c4f9a1654
Arunanshu Biswas <mydellpc07@gmail.com>
2020-07-10 17:13:41 +0530

    Added documentation to Hash interface

--- pyflocker/ciphers/interfaces/Hash.py
+++ pyflocker/ciphers/interfaces/Hash.py
@@ -2,4 +2,10 @@ from .. import load_cipher as _load_cpr
 
 
 def new(hashname, data=b'', *, backend=None):
+    """
+    Instantiate a new hash instance `hashname` with initial
+    data `data` (default is empty `bytes`).
+
+    `backend` must be an attribute of `Backends`.
+    """
     return _load_cpr('Hash', backend).Hash(hashname, data)
