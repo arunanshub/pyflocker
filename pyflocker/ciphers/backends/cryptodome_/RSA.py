c2ac890f61a862f8dd68e719fdcca9cea909bef6
Arunanshu Biswas <mydellpc07@gmail.com>
2020-07-26 20:00:50 +0530

    Cryptodome's RSA signing compatible with pyca/cryptography

--- pyflocker/ciphers/backends/cryptodome_/RSA.py
+++ pyflocker/ciphers/backends/cryptodome_/RSA.py
@@ -283,7 +283,23 @@ class RSADecryptionCtx(CipherContext):
 def _get_signer(key, pad):
     _pad, _mgf = _get_padding(pad)
     if pad.salt_len is None:
-        return _pad(key, mgfunc=_mgf)
+
+        # create a custom sign/verify wrapper to preserve consistency:
+        # pyca/cryptography follows the OpenSSL quirk where the default
+        # salt length is maximized and doesn't match with the size of the
+        # digest applied to the message.
+        def sign_or_verify(msghash, signature=None):
+            salt_len = key.size_in_bytes() - msghash.digest_size - 2
+            sigver = _pad(key, mgfunc=_mgf, saltLen=salt_len)
+            if signature is None:
+                return sigver.sign(msghash)
+            return sigver.verify(msghash, signature)
+
+        # create an object placeholder object to shadow the signer/verifier.
+        funcs = dict(sign=sign_or_verify, verify=sign_or_verify)
+        svobj = type('_SigVerCtx', (), funcs)
+        return svobj
+
     return _pad(key, mgfunc=_mgf, saltLen=pad.salt_len)
 
 
