b333cc2bb1445ce4ddf9a8d4efa84ee8efe06ba0
Arunanshu Biswas <mydellpc07@gmail.com>
2020-08-07 01:25:58 +0530

    version increment

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='PyFLocker',
 
-    version='0.1.7',
+    version='0.1.8',
     author="Arunanshu Biswas",
     author_email="mydellpc07@gmail.com",
 
