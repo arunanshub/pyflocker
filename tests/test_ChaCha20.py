8ecf23588258e352e19bd3a68cb15e1f3d540692
Arunanshu Biswas <mydellpc07@gmail.com>
2020-09-15 01:07:58 +0530

    Add test for ChaCha20

---
+++ tests/test_ChaCha20.py
@@ -0,0 +1,28 @@
+import os
+import pytest
+
+from functools import partial
+from pyflocker.ciphers import ChaCha20, Backends
+
+from .base import BaseSymmetric
+
+
+@pytest.fixture
+def cipher(nonce_length):
+    return partial(
+        ChaCha20.new,
+        key=os.urandom(32),
+        nonce=os.urandom(nonce_length)
+    )
+
+
+@pytest.mark.parametrize(
+    'nonce_length',
+    [8, 12],
+)
+@pytest.mark.parametrize(
+    'backend',
+    list(Backends),
+)
+class TestChaCha20Poly1305(BaseSymmetric):
+    pass
