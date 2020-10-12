3cfd9bbbde981ebfdd6c6810e86f8f6356688a2a
Arunanshu Biswas <mydellpc07@gmail.com>
2020-10-12 23:18:11 +0530

    Add some more tests for Hash

--- tests/test_Hash.py
+++ tests/test_Hash.py
@@ -1,6 +1,8 @@
+from itertools import permutations
 import pytest
 
 from pyflocker.ciphers import Hash, Backends
+from pyflocker.ciphers import exc
 
 
 def create_hash(algo, data, *, backend, **kwargs):
@@ -25,3 +27,23 @@ class TestHash:
         h2 = create_hash(algo, data, backend=backend)
 
         assert h1.digest() == h2.digest()
+
+        with pytest.raises(exc.AlreadyFinalized):
+            h1.update(data)
+
+    @pytest.mark.parametrize(
+        "backends",
+        permutations(list(Backends), 2),
+    )
+    def test_same_hash_crossed(self, algo, backends, *, backend):
+        # 'backend' is not used since we are cross checking backends
+        b1, b2 = backends
+        data = bytes(64)
+        h1 = create_hash(algo, data, backend=b1)
+        h2 = create_hash(algo, data, backend=b2)
+
+        assert h1.digest() == h2.digest()
+
+        for h in [h1, h2]:
+            with pytest.raises(exc.AlreadyFinalized):
+                h.update(data)
