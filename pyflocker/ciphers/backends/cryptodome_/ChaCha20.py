6716b935049b7a4e13e20ac6d101919c238e533e
Arunanshu Biswas <mydellpc07@gmail.com>
2021-02-06 11:51:09 +0530

    remove unused import

--- pyflocker/ciphers/backends/cryptodome_/ChaCha20.py
+++ pyflocker/ciphers/backends/cryptodome_/ChaCha20.py
@@ -3,7 +3,6 @@ import typing
 from Cryptodome.Cipher import ChaCha20 as _ChaCha20
 from Cryptodome.Cipher import ChaCha20_Poly1305 as _ChaCha20_Poly1305
 
-from ... import base
 from ..symmetric import FileCipherWrapper
 from .symmetric import AEADCipherTemplate, NonAEADCipherTemplate
 
