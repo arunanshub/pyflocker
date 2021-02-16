162caab058f74dca4670daa03d0b971093ebc93e
Arunanshu Biswas <mydellpc07@gmail.com>
2021-02-16 19:17:09 +0530

    Add asymmetric tools for Cryptodome

---
+++ pyflocker/ciphers/backends/cryptodome_/asymmetric.py
@@ -0,0 +1,88 @@
+from functools import partial
+from typing import MappingProxyType
+
+from Cryptodome.Cipher import PKCS1_OAEP
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Signature import pss
+
+from .. import asymmetric
+
+
+def get_OAEP(key, padding):
+    """Construct a Cryptodome specific OAEP object.
+
+    Args:
+        key: Public/Private key (from the Cryptodome backend).
+        padding (OAEP): An OAEP object.
+
+    Returns:
+        OAEP object:
+            An OAEP encryptor/decryptor object depending on the key, from the
+            Cryptodome backend.
+    """
+    if isinstance(mgf := padding.mgf, asymmetric.MGF1):
+        mgf = partial(PKCS1_OAEP.MGF1, hash_gen=padding.mgf.hashfunc.new())
+
+    return PKCS1_OAEP.new(
+        key,
+        padding.hashfunc.new(),
+        mgf,
+        padding.label or b"",
+    )
+
+
+def get_PSS(key, padding):
+    """Construct a Cryptodome specific PSS object.
+
+    Args:
+        key: Public/Private key (from the Cryptodome backend).
+        padding (PSS): A PSS object.
+
+    Returns:
+        PSS object: An PSS signer/verifier object, depending on the key.
+    """
+    if isinstance(mgf := padding.mgf, asymmetric.MGF1):
+        mgf = partial(PKCS1_OAEP.MGF1, hash_gen=padding.mgf.hashfunc.new())
+
+    if padding.salt_length is None:
+        return _SaltLengthMaximizer(key, padding)
+
+    return pss.new(
+        key,
+        mask_func=mgf,
+        salt_bytes=padding.salt_length,
+    )
+
+
+class _SaltLengthMaximizer:
+    def __init__(self, key, padding):
+        self._key = key
+        self._padding = padding
+
+    def _sign_or_verify(self, msghash, signature=None):
+        salt_length = self._key.size_in_bytes() - msghash.digest_size - 2
+        pss = get_PSS(
+            self._key,
+            type(self._padding)(self._padding.mgf, salt_length),
+        )
+        if signature is None:
+            return pss.sign(msghash)
+        return pss.verify(msghash, signature)
+
+    def sign(self, msghash):
+        if not self._key.has_private():
+            raise TypeError("The key is not a private key.")
+        return self._sign_or_verify(msghash)
+
+    def verify(self, msghash, signature):
+        return self._sign_or_verify(msghash, signature)
+
+
+PADDINGS = MappingProxyType(
+    {
+        asymmetric.OAEP: get_OAEP,
+        asymmetric.PSS: get_PSS,
+    }
+)
+
+del MappingProxyType
