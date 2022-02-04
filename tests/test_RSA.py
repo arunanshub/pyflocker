47092e5230edb3ce72d08bd38b0ed5a2ef0a089d
Arunanshu Biswas <mydellpc07@gmail.com>
2022-02-04 20:44:16 +0530

    test(test_RSA): add RSA encoding tests for private key

--- tests/test_RSA.py
+++ tests/test_RSA.py
@@ -1,5 +1,5 @@
 import hashlib
-from itertools import combinations_with_replacement as repcomb
+from itertools import product
 
 import pytest
 
@@ -8,28 +8,68 @@ from pyflocker.ciphers.backends import Backends
 
 SERIALIZATION_KEY = hashlib.sha256(b"SERIALIZATION_KEY").digest()
 
+ENCRYPTION_PASSPHRASE = hashlib.sha256(b"ENCRYPTION_PASSPHRASE").digest()
 
-@pytest.mark.parametrize("key_size", [1024, 2048, 4096])
-@pytest.mark.parametrize("backend1, backend2", list(repcomb(Backends, 2)))
-def test_private_key_serde(
-    key_size: int,
-    backend1: Backends,
-    backend2: Backends,
-):
-    priv_key = RSA.generate(key_size, backend=backend1)
-    serialized = priv_key.serialize(passphrase=SERIALIZATION_KEY)
 
-    with pytest.raises(ValueError):
-        RSA.load_private_key(serialized, b"incorrect key", backend=backend2)
+@pytest.fixture
+def private_key(bits: int, backend: Backends):
+    return RSA.generate(bits, backend=backend)
 
-    priv_key_2 = RSA.load_private_key(
-        serialized,
-        SERIALIZATION_KEY,
-        backend=backend2,
-    )
 
-    assert priv_key.n == priv_key_2.n  # type: ignore
-    assert priv_key.e == priv_key_2.e  # type: ignore
-    assert priv_key.p == priv_key_2.p  # type: ignore
-    assert priv_key.q == priv_key_2.q  # type: ignore
-    assert priv_key.d == priv_key_2.d  # type: ignore
+@pytest.mark.parametrize("bits", [1024, 2048])
+@pytest.mark.parametrize(
+    "backend, backend2",
+    list(product(Backends, repeat=2)),
+)
+class TestPrivateKeyEncoding:
+    @pytest.mark.parametrize("format", ["PKCS1", "PKCS8", "OpenSSH"])
+    @pytest.mark.parametrize("passphrase", [None, ENCRYPTION_PASSPHRASE])
+    def test_PEM(
+        self,
+        private_key,
+        format: str,
+        backend: Backends,
+        backend2: Backends,
+        passphrase,
+    ):
+        try:
+            serialized = private_key.serialize(
+                encoding="PEM",
+                format=format,
+                passphrase=passphrase,
+            )
+        except ValueError:
+            assert backend == Backends.CRYPTODOME and format == "OpenSSH"
+            return pytest.skip(f"{backend} does not support format {format}")
+
+        RSA.load_private_key(
+            serialized,
+            backend=backend2,
+            passphrase=passphrase,
+        )
+
+    @pytest.mark.parametrize("format", ["PKCS1", "PKCS8"])
+    @pytest.mark.parametrize("passphrase", [None, ENCRYPTION_PASSPHRASE])
+    def test_DER(
+        self,
+        private_key,
+        format: str,
+        backend: Backends,
+        backend2: Backends,
+        passphrase,
+    ):
+        try:
+            serialized = private_key.serialize(
+                encoding="DER",
+                format=format,
+                passphrase=passphrase,
+            )
+        except ValueError:
+            assert format == "PKCS1"
+            return
+
+        RSA.load_private_key(
+            serialized,
+            backend=backend2,
+            passphrase=passphrase,
+        )
