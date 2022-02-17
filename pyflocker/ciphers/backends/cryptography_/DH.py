110da1f0fdc42e1fa44b4b96afef9b771cfb3290
Arunanshu Biswas <mydellpc07@gmail.com>
2022-02-18 00:29:34 +0530

    feat(cryptography/DH): get parameter numbers in __init__; add `key_size` property
    Additionally, refactor docs and `load` function

--- pyflocker/ciphers/backends/cryptography_/DH.py
+++ pyflocker/ciphers/backends/cryptography_/DH.py
@@ -1,18 +1,7 @@
 from __future__ import annotations
 
-import sys
 import typing
 
-if sys.version_info >= (3, 9):
-    from functools import cache as _cache
-else:
-    from functools import lru_cache  # pragma: no cover
-
-    def _cache(func):  # pragma: no cover
-        return lru_cache(maxsize=None)(func)
-
-
-from cryptography.hazmat.backends import default_backend as defb
 from cryptography.hazmat.primitives import serialization as ser
 from cryptography.hazmat.primitives.asymmetric import dh
 
@@ -33,17 +22,33 @@ class DHParameters:
                 raise ValueError("The parameter is not a DH parameter object.")
             self._params = params
         else:
-            self._params = dh.generate_parameters(
-                generator,
-                key_size,
-                defb(),
-            )
+            self._params = dh.generate_parameters(generator, key_size)
+
+        numbers = self._params.parameter_numbers()
+        self._g = numbers.g
+        self._p = numbers.p
+        self._q = numbers.q
+
+    @property
+    def g(self) -> int:
+        """The generator value."""
+        return self._g
+
+    @property
+    def p(self) -> int:
+        """The prime modulus value."""
+        return self._p
+
+    @property
+    def q(self) -> typing.Optional[int]:
+        """The p subgroup order value."""
+        self._q
 
     def private_key(self) -> DHPrivateKey:
         """Create a DH private key from the parameters.
 
         Returns:
-            DHPrivateKey: A private key object.
+            A private key object.
         """
         return DHPrivateKey(self._params.generate_private_key())
 
@@ -51,12 +56,12 @@ class DHParameters:
         """Serialize the DH parameters.
 
         Args:
-            encoding (str):
+            encoding:
                 The encoding can be ``PEM`` or ``DER``. Defaults to ``PEM``.
-            format (str): The format. Defaults to ``PKCS3``.
+            format: The format. Defaults to ``PKCS3``.
 
         Returns:
-            bytes: The parameters encoded as bytes object.
+            The parameters encoded as bytes object.
 
         Raises:
             ValueError: if the encoding of format is invalid.
@@ -67,53 +72,35 @@ class DHParameters:
                 PARAMETER_FORMATS[format],
             )
         except KeyError as e:
-            raise ValueError(f"Invalid encoding or format: {e.args}") from e
-
-    def _numbers(self):
-        return self._params.parameter_numbers()
-
-    @property
-    @_cache
-    def g(self) -> int:
-        """The generator value."""
-        return self._numbers().g
-
-    @property
-    @_cache
-    def p(self) -> int:
-        """The prime modulus value."""
-        return self._numbers().p
-
-    @property
-    def q(self) -> int:
-        """The p subgroup order value."""
-        return self._numbers().q
+            raise ValueError(f"Invalid encoding or format: {e.args[0]}") from e
 
     @classmethod
     def load(cls, data: bytes) -> DHParameters:
-        """Load the :any:`DHParameters` from the encoded format.
+        """Deserialize the encoded DH parameters.
 
         Args:
-            data (bytes, bytearray):
+            data:
                 The parameters as an encoded bytes object.
 
         Returns:
             DHParameters: DH parameter object.
         """
-        fmts = {
+        formats = {
             b"-----BEGIN DH PARAMETERS": ser.load_pem_parameters,
             b"0": ser.load_der_parameters,
         }
 
         try:
-            loader = fmts[[*filter(data.startswith, fmts)][0]]
-        except IndexError:
+            loader = formats[next(filter(data.startswith, formats))]
+        except StopIteration:
             raise ValueError("Invalid format.") from None
+
         try:
-            params = loader(data, defb())
+            params = loader(data)
             if not isinstance(params, dh.DHParameters):
                 raise ValueError("Invalid parameter format.")
-            return cls(None, parameter=params)
+            # 1024 will not be used anyways; keeps mypy happy
+            return cls(1024, parameter=params)
         except ValueError as e:
             raise ValueError(
                 "Cannot deserialize key. Either Key format is invalid or "
@@ -130,64 +117,71 @@ class DHParameters:
         """Generates a DH parameter group from the parameters.
 
         Args:
-            p (int): The prime modulus value.
-            g (int): The generator value. Must be 2 or 5. Default is 2.
-            q (int): p subgroup order value. Defaults to ``None``.
+            p: The prime modulus value.
+            g: The generator value. Must be 2 or 5. Default is 2.
+            q: p subgroup order value. Defaults to ``None``.
 
         Returns:
-            DHParameters: DHParameters object.
+            DHParameters object.
         """
         param_nos = dh.DHParameterNumbers(p, g, q)
-        return cls(None, parameter=param_nos.parameters(defb()))
+        return cls(1024, parameter=param_nos.parameters())
 
 
-class _DHKey:
+class DHPrivateKey(base.BasePrivateKey):
+    def __init__(self, key):
+        if not isinstance(key, dh.DHPrivateKey):
+            raise ValueError("The key is not a DH private key.")
+        self._key = key
+
+        numbers = key.private_numbers()
+        self._x = numbers.x
+
     def parameters(self) -> DHParameters:
         """Creates a new :any:`DHParameters` object from the key.
 
         Returns:
-            DHParameters: The DH parameter object.
+            The DH parameter object.
         """
-        return DHParameters(None, parameter=self._key.parameters())
+        return DHParameters(1024, parameter=self._key.parameters())
 
     @property
     def key_size(self) -> int:
         """Size of the key, in bytes."""
         return self._key.key_size
 
-
-class DHPrivateKey(_DHKey, base.BasePrivateKey):
-    def __init__(self, key):
-        if not isinstance(key, dh.DHPrivateKey):
-            raise ValueError("The key is not a DH private key.")
-        self._key = key
-
     def public_key(self) -> DHPublicKey:
         """Create a public key from the private key.
 
         Returns:
-            DHPublicKey: A public key object.
+            A public key object.
         """
         return DHPublicKey(self._key.public_key())
 
-    def exchange(self, peer_public_key: bytes) -> bytes:
+    def exchange(
+        self,
+        peer_public_key: typing.Union[bytes, DHPublicKey],
+    ) -> bytes:
         """Perform a key exchange.
 
         Args:
-            peer_public_key (bytes, bytearray):
+            peer_public_key:
                 The peer public key can be a bytes or a :any:`DHPublicKey`
                 object.
 
         Returns:
-            bytes: A shared key.
+            A shared key.
 
         Raises:
-            TypeError: if ``peer_public_key`` is not a bytes-like object.
+            TypeError:
+                if ``peer_public_key`` is not a bytes-like or
+                :any:`DHPublicKey` object.
         """
-        if not isinstance(peer_public_key, (bytes, bytearray, memoryview)):
-            raise TypeError("peer_public_key must be a bytes-like object.")
-        peer_public_key = DHPublicKey.load(peer_public_key)
-        return self._key.exchange(peer_public_key._key)
+        if isinstance(peer_public_key, DHPublicKey):
+            return self._key.exchange(peer_public_key._key)
+        return self._key.exchange(
+            DHPublicKey.load(memoryview(peer_public_key))._key
+        )
 
     def serialize(
         self,
@@ -214,25 +208,24 @@ class DHPrivateKey(_DHKey, base.BasePrivateKey):
             TypeError: if the passphrase is not a bytes-like object.
         """
         if passphrase is None:
-            prot = ser.NoEncryption()
+            protection = ser.NoEncryption()
         else:
-            if not isinstance(passphrase, (bytes, bytearray, memoryview)):
-                raise TypeError("passphrase must be a bytes-like object.")
-            prot = ser.BestAvailableEncryption(passphrase)
+            protection = ser.BestAvailableEncryption(
+                memoryview(passphrase).tobytes()
+            )
 
         try:
             return self._key.private_bytes(
                 ENCODINGS[encoding],
                 PRIVATE_FORMATS[format],
-                prot,
+                protection,
             )
         except KeyError as e:
-            raise ValueError(f"Invalid encoding or format: {e.args}") from e
+            raise ValueError(f"Invalid encoding or format: {e.args[0]}") from e
 
     @property
-    @_cache
     def x(self) -> int:
-        return self._key.private_numbers().x
+        return self._x
 
     @classmethod
     def load(
@@ -243,39 +236,34 @@ class DHPrivateKey(_DHKey, base.BasePrivateKey):
         """Deserialize and load the the private key.
 
         Args:
-            data (bytes): The serialized private key as ``bytes-like`` object.
-            passphrase (bytes, bytearray):
-                The passphrase that was used to protect the private key.
-                If key is not protected, passphrase is ``None``.
+            data: The serialized private key as ``bytes-like`` object.
+            passphrase:
+                The passphrase that was used to protect the private key. If key
+                is not protected, passphrase is ``None``.
 
         Returns:
-            DHPrivateKey: A private key.
+            A private key.
 
         Raises:
             ValueError: If the key could not be deserialized.
             TypeError: If passphrase is not a bytes-like object.
         """
-        fmts = {
+        formats = {
             b"-----": ser.load_pem_private_key,
             b"0": ser.load_der_private_key,
         }
 
         try:
-            loader = fmts[[*filter(data.startswith, fmts)][0]]
-        except IndexError:
+            loader = formats[next(filter(data.startswith, formats))]
+        except StopIteration:
             raise ValueError("Invalid format.") from None
 
         # type check
         if passphrase is not None:
-            if not isinstance(passphrase, (bytes, bytearray, memoryview)):
-                raise TypeError("passphrase must be a bytes-like object.")
+            passphrase = memoryview(passphrase).tobytes()
 
         try:
-            key = loader(
-                memoryview(data),
-                passphrase,
-                defb(),
-            )
+            key = loader(memoryview(data).tobytes(), passphrase)
             return cls(key)
         except ValueError as e:
             raise ValueError(
@@ -290,11 +278,25 @@ class DHPrivateKey(_DHKey, base.BasePrivateKey):
             ) from e
 
 
-class DHPublicKey(_DHKey, base.BasePublicKey):
+class DHPublicKey(base.BasePublicKey):
     def __init__(self, key):
         if not isinstance(key, dh.DHPublicKey):
             raise ValueError("The key is not a DH public key.")
         self._key = key
+        self._y = key.public_numbers().y
+
+    def parameters(self) -> DHParameters:
+        """Creates a new :any:`DHParameters` object from the key.
+
+        Returns:
+            The DH parameter object.
+        """
+        return DHParameters(1024, parameter=self._key.parameters())
+
+    @property
+    def key_size(self) -> int:
+        """Size of the key, in bytes."""
+        return self._key.key_size
 
     def serialize(
         self,
@@ -304,11 +306,11 @@ class DHPublicKey(_DHKey, base.BasePublicKey):
         """Serialize the public key.
 
         Args:
-            encoding (str): The encoding to use. It can be ``PEM`` or ``DER``.
-            format (str): The format can be ``SubjectPublicKeyInfo`` only.
+            encoding: The encoding to use. It can be ``PEM`` or ``DER``.
+            format: The format can be ``SubjectPublicKeyInfo`` only.
 
         Returns:
-            bytes: The public key as bytes object.
+            The public key as bytes object.
 
         Raises:
             KeyError: if the encoding or format is invalid.
@@ -322,53 +324,48 @@ class DHPublicKey(_DHKey, base.BasePublicKey):
             raise ValueError(f"Invalid encoding or format: {e.args}") from e
 
     @property
-    @_cache
     def y(self) -> int:
-        return self._key.public_numbers().y
+        return self._y
 
     @classmethod
     def load(cls, data: bytes) -> DHPublicKey:
         """Deserialize and load the public key.
 
         Args:
-            data (bytes): The serialized public key as ``bytes-like`` object.
+            data: The serialized public key as ``bytes-like`` object.
 
         Returns:
-            DHPublicKey: A public key object.
+            A public key object.
 
         Raises:
             ValueError: If the key could not be deserialized.
         """
-        fmts = {
+        formats = {
             b"-----": ser.load_pem_public_key,
             b"0": ser.load_der_public_key,
         }
 
         try:
-            loader = fmts[[*filter(data.startswith, fmts)][0]]
+            loader = formats[next(filter(data.startswith, formats))]
         except IndexError:
             raise ValueError("Invalid format.") from None
 
         try:
-            key = loader(memoryview(data), defb())
-            return cls(key=key)
+            return cls(key=loader(memoryview(data)))
         except ValueError as e:
             raise ValueError(
                 "Cannot deserialize key. Incorrect key format.",
             ) from e
 
 
-def generate(
-    key_size: int,
-    g: int = 2,
-) -> DHParameters:
+def generate(key_size: int, g: int = 2) -> DHParameters:
     """
     Generate DHE parameter with prime number's bit size ``bits`` and
     generator ``g`` (default 2). Recommended size of ``bits`` > 1024.
 
     Args:
-        key_size (int): The bit length of the prime modulus.
-        g (int): The value to use as a generator value. Default is 2.
+        key_size: The bit length of the prime modulus.
+        g: The value to use as a generator value. Default is 2.
 
     Returns:
         DHParameters: A DH key exchange paramenter object.
@@ -384,9 +381,9 @@ def load_from_parameters(
     """Create a DH Parameter object from the given parameters.
 
     Args:
-        p (int): The prime modulus ``p`` as ``int``.
-        g (int): The generator.
-        q (int): ``p`` subgroup order value.
+        p: The prime modulus ``p`` as ``int``.
+        g: The generator.
+        q: ``p`` subgroup order value.
 
     Returns:
         DHParameters: A DH key exchange paramenter object.
@@ -398,7 +395,7 @@ def load_parameters(data: bytes) -> DHParameters:
     """Deserialize the DH parameters and load a parameter object.
 
     Args:
-        data (bytes): Serialized DH Parameter.
+        data: Serialized DH Parameter.
 
     Returns:
         DHParameters: A parameter object.
@@ -410,15 +407,10 @@ def load_public_key(data: bytes) -> DHPublicKey:
     """Loads the public key and returns a Key interface.
 
     Args:
-        data (bytes, bytearray):
-            The public key (a bytes-like object) to deserialize.
-
-    Keyword Arguments:
-        backend (:class:`pyflocker.ciphers.backends.Backends`):
-            The backend to use. It must be a value from :any:`Backends`.
+        data: The public key (a bytes-like object) to deserialize.
 
     Returns:
-        DHPublicKey: A public key object.
+        A public key object.
     """
     return DHPublicKey.load(data)
 
@@ -434,17 +426,12 @@ def load_private_key(
     object.
 
     Args:
-        data (bytes, bytearray):
-            The private key (a bytes-like object) to deserialize.
-        passphrase (bytes, bytearray):
-            The passphrase (in bytes) that was used to encrypt the
-            private key.`None` if the key was not encrypted.
-
-    Keyword Arguments:
-        backend (:class:`pyflocker.ciphers.backends.Backends`):
-            The backend to use. It must be a value from :any:`Backends`.
+        data: The private key (a bytes-like object) to deserialize.
+        passphrase:
+            The passphrase (in bytes) that was used to encrypt the private
+            key. ``None`` if the key was not encrypted.
 
     Returns:
-        DHPrivateKey: A private key object.
+        A private key object.
     """
     return DHPrivateKey.load(data, passphrase)
