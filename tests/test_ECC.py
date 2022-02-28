9d2b280ebb378093818b3b26683111c016acd62c
Arunanshu Biswas <mydellpc07@gmail.com>
2022-03-01 01:50:33 +0530

    test(test_ECC): add ECC error tests

--- tests/test_ECC.py
+++ tests/test_ECC.py
@@ -14,6 +14,65 @@ ENCRYPTION_PASSPHRASE = hashlib.sha256(b"ENCRYPTION_PASSPHRASE").digest()
 
 SIGNING_DATA = b"SIGNING_DATA for SignerContext and VerifierContext"
 
+# an encrypted DH private key. passphrase is "helloworld"
+TESTING_DH_PRIVATE_KEY = b"""\
+-----BEGIN ENCRYPTED PRIVATE KEY-----
+MIIBjTBXBgkqhkiG9w0BBQ0wSjApBgkqhkiG9w0BBQwwHAQIzowzofKfAGACAggA
+MAwGCCqGSIb3DQIJBQAwHQYJYIZIAWUDBAEqBBArFYSwsP4mEyCu79uP+GOZBIIB
+MK4sOioQz0algdwTMI6gSJ+5FvmuxLxBiSp02gUDrpZoHRs/K7NSZ6AeTPvVUYBO
+/C3Vjg3aJYVyK4V2elhjVOfpq2n9eIfBwHvu+NGH8NZdOGmi2/jK4yDSzcZJF9Bh
+jcINW5DqjyZiCBhvJgzBtYKs0XuQ0Yc/xE/loy5qufP7rxSdzMHr+AuwOSUj5LVO
+AFzqrQlPn8j2mNAw6YKDNKoFNq4S8bIhktBo3+FBcYDk02iDf47eNodJB5wQpWug
+iu6pl6Yr+OpIwD3hpKi+73BegAKT5uc23tHX9o6Oa9j8eLJz5TMgfrSHii9MDUZQ
+H3RGMEsP1mfW81R0UiPKlufCwJ/wqFfkTymGdpemYu0L4E7tj8AdiA0JD23Wui+c
+UuB24iHAujMntADTlMDI83g=
+-----END ENCRYPTED PRIVATE KEY-----
+"""
+
+TESTING_DH_PUBLIC_KEY = b"""\
+-----BEGIN PUBLIC KEY-----
+MIIBIDCBlQYJKoZIhvcNAQMBMIGHAoGBANpLQ3kmQ//xRiBjnsiohGcJc/HpRxEp
+KC1Io8GSiXl8XeABrRvWYai3rY7ylFrupfWfEP8vBC1uf2lHm7ZNFQOnTsLCUpw3
+BlORkP/H80tNjhn1CA7YwGMxF3DYuxDmKUGJcpucvErVf5Y/KbFlE7fbfXtoEeCK
+Gduu/aUWo1/bAgECA4GFAAKBgQCh2cr189Ag1wowpYNDRsk79L4KRidXYJbGlDsB
+eRrZ02861z2aWNtwjqFMlGTdbikJ8zCYV/wFzDkPyBHY8UFwyvvZMhCcL7aP+ZJb
+TuIWaJnduPv+7NnVXztyz0Jqk+GPrmiAhaOpZdDc2XvUQeuzNuYlg9oDTgWobuMX
+c42Kpw==
+-----END PUBLIC KEY-----
+"""
+
+TESTING_ENCRYPTED_PRIVATE_KEY = b"""\
+-----BEGIN ENCRYPTED PRIVATE KEY-----
+MIIBXTBXBgkqhkiG9w0BBQ0wSjApBgkqhkiG9w0BBQwwHAQIiPDccoM2IRkCAggA
+MAwGCCqGSIb3DQIJBQAwHQYJYIZIAWUDBAEqBBAI99JBsmRKT1udO2hzIBVJBIIB
+AORDEPZRIvR8Q6803Qo47vT6Dv9HYMkrlWkWoJb5Rtns/w9rAbFxTV5QbwKFHYmu
++4PHlOvygGqnHD0BHNfa7iXcj4J1JCaLKDVMcCKwRFd6k66pvuRdlr+o0t+p7OAN
+sC4OyFFCoW2IIv0FhYOOjGRjZ/SIP0XFeJoNkCGPW+RuvhuoxqQs4nsmDUHBMZ/a
+i1vLX7IwCEGzQ1yuh98qpskyz7rLfVl3HXOC5Up0OaIb0RFWhASZGk5dSHiZ7xyU
+pWhSJb5SVMeRcwA9dMGAUI6cMkmtUz2L+0aD6fhlFWg7pxdERTiBul63dMPOnvLL
+xs87hw8M5asrMuELxcFJh9s=
+-----END ENCRYPTED PRIVATE KEY-----
+"""
+
+TESTING_UNENCRYPTED_PRIVATE_KEY = b"""\
+-----BEGIN PRIVATE KEY-----
+MIHuAgEAMBAGByqGSM49AgEGBSuBBAAjBIHWMIHTAgEBBEIBU0YHkKbrufzQYQ1n
+JtD03T/tbA1TYxQAyJdvfWNSoCEN1DqfmEvAJiw0fCgGjNGvRQ6jNhr2LPhwPOch
+DQ29uC2hgYkDgYYABAF6iFlfWiA0Obc05eOaZEy5kBe7Fep8/vi7w3mYZkqV7Yvv
+w1J/8ACaxhXuJOr+x6lcL/4e4k0apwEh0y/6qecdywB2C+Vvf0ELwuNtEP0PsY+s
+F2JEONee3Cc4h0wdXhRrPlT4F1ZkYdDKdqDSY/4HSPzKxDJNd8J/Xf7GLoQ3aO5F
+LQ==
+-----END PRIVATE KEY-----
+"""
+
+TESTING_PUBLIC_KEY = b"""\
+-----BEGIN PUBLIC KEY-----
+MIGbMBAGByqGSM49AgEGBSuBBAAjA4GGAAQBeohZX1ogNDm3NOXjmmRMuZAXuxXq
+fP74u8N5mGZKle2L78NSf/AAmsYV7iTq/sepXC/+HuJNGqcBIdMv+qnnHcsAdgvl
+b39BC8LjbRD9D7GPrBdiRDjXntwnOIdMHV4Uaz5U+BdWZGHQynag0mP+B0j8ysQy
+TXfCf13+xi6EN2juRS0=
+-----END PUBLIC KEY-----
+"""
 
 if typing.TYPE_CHECKING:
     from pyflocker.ciphers import base
@@ -200,3 +259,112 @@ class TestECCExchange:
         except NotImplementedError:
             assert Backends.CRYPTODOME in (backend, backend2)
             return pytest.skip("Key exchange not supported by Cryptodome")
+
+    def test_exchange_key_ECDH(
+        self,
+        private_key: base.BaseECCPrivateKey,
+        backend,
+        backend2,
+    ):
+        algorithm = ECDH()
+
+        try:
+            private_key2 = ECC.generate(private_key.curve, backend=backend2)
+            assert private_key.exchange(
+                private_key2.public_key(),
+                algorithm=algorithm,
+            ) == private_key2.exchange(
+                private_key.public_key(),
+                algorithm=algorithm,
+            )
+        except NotImplementedError:
+            assert Backends.CRYPTODOME in (backend, backend2)
+            return pytest.skip("Key exchange not supported by Cryptodome")
+
+
+curve_p256_fixture = pytest.mark.parametrize("curve", ["p256"], scope="module")
+
+
+@pytest.mark.parametrize("backend", list(Backends), scope="module")
+class TestECCErrors:
+    def test_invalid_curve_name(self, backend):
+        with pytest.raises(ValueError):
+            ECC.generate("invalid-curve", backend=backend)
+
+    @curve_p256_fixture
+    def test_private_key_invalid_encoding_format(self, private_key):
+        with pytest.raises(ValueError):
+            private_key.serialize(encoding="nonexistent")
+        with pytest.raises(ValueError):
+            private_key.serialize(format="nonexistent")
+
+    @curve_p256_fixture
+    def test_public_key_serialize_invalid_encoding_format(self, private_key):
+        public_key = private_key.public_key()
+        with pytest.raises(ValueError):
+            public_key.serialize(encoding="nonexistent")
+        with pytest.raises(ValueError):
+            public_key.serialize(format="nonexistent")
+
+    def test_private_key_load_invalid_data(self, backend):
+        with pytest.raises(ValueError):
+            ECC.load_private_key(b"invalid-data", backend=backend)
+        with pytest.raises(ValueError):
+            ECC.load_private_key(
+                b"invalid-data",
+                backend=backend,
+                passphrase=b"invalid",
+            )
+
+    def test_private_key_load_invalid_password(self, backend):
+        with pytest.raises(ValueError):
+            ECC.load_private_key(
+                TESTING_ENCRYPTED_PRIVATE_KEY,
+                backend=backend,
+            )
+        with pytest.raises(ValueError):
+            ECC.load_private_key(
+                TESTING_ENCRYPTED_PRIVATE_KEY,
+                passphrase=b"nothepassphrase",
+                backend=backend,
+            )
+            ECC.load_private_key(
+                TESTING_UNENCRYPTED_PRIVATE_KEY,
+                passphrase=b"nothepassphrase",
+                backend=backend,
+            )
+        with pytest.raises(ValueError):
+            ECC.load_private_key(
+                TESTING_DH_PRIVATE_KEY,
+                passphrase=b"helloworld",
+                backend=backend,
+            )
+        with pytest.raises(ValueError):
+            ECC.load_private_key(TESTING_PUBLIC_KEY, backend=backend)
+
+    def test_public_key_load_invalid_data(self, backend):
+        with pytest.raises(ValueError):
+            ECC.load_public_key(b"invalid-data", backend=backend)
+        with pytest.raises(ValueError):
+            ECC.load_public_key(
+                TESTING_ENCRYPTED_PRIVATE_KEY,
+                backend=backend,
+            )
+        with pytest.raises(ValueError):
+            ECC.load_public_key(
+                TESTING_UNENCRYPTED_PRIVATE_KEY,
+                backend=backend,
+            )
+        with pytest.raises(ValueError):
+            ECC.load_public_key(
+                TESTING_DH_PUBLIC_KEY,
+                backend=backend,
+            )
+
+    @curve_p256_fixture
+    def test_public_key_openssh_not_with_openssh(self, private_key):
+        public_key = private_key.public_key()
+        with pytest.raises(ValueError):
+            public_key.serialize("OpenSSH", "SubjectPublicKeyInfo")
+        with pytest.raises(ValueError):
+            public_key.serialize("PEM", "OpenSSH")
