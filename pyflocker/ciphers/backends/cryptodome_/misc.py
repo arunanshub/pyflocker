480dfd79f607a5202a8c19ddcb69984a347ba1f6
Arunanshu Biswas <mydellpc07@gmail.com>
2022-05-10 20:28:04 +0530

    feat(misc): accept only types implementing `BaseHash`

--- pyflocker/ciphers/backends/cryptodome_/misc.py
+++ pyflocker/ciphers/backends/cryptodome_/misc.py
@@ -7,13 +7,12 @@ from __future__ import annotations
 from Cryptodome.Protocol import KDF
 
 from ...base import BaseHash
-from . import Hash
 
 
 def derive_hkdf_key(
     master_key: bytes,
     dklen: int,
-    hashalgo: str | BaseHash,
+    hashalgo: BaseHash,
     salt: bytes,
     cipher_ctx: bytes = b"enc-key",
     auth_ctx: bytes = b"auth-key",
@@ -21,32 +20,26 @@ def derive_hkdf_key(
     """Derive key materials for HMAC from given master key.
 
     Args:
-        master_key (bytes): The key used to derive the keys from.
-        dklen (int): Desired lenth of the derived key.
-        hashalgo (str, BaseHash): The name of the hash algorithm.
-        salt (bytes): The salt to use.
-        cipher_ctx (bytes): Context for cipher.
-        auth_ctx (bytes): Context for HMAC.
+        master_key: The key used to derive the keys from.
+        dklen: Desired lenth of the derived key.
+        hashalgo: The name of the hash algorithm.
+        salt: The salt to use.
+        cipher_ctx: Context for cipher.
+        auth_ctx: Context for HMAC.
 
     Returns:
-        tuple[bytes, bytes]: A pair of *cipher key* and *MAC key*.
+        A pair of *cipher key* and *MAC key*.
     """
-    if not isinstance(hashalgo, (str, BaseHash)):
-        raise TypeError(
-            "hashalgo must be a str or an object implementing BaseHash."
-        )
+    if not isinstance(hashalgo, BaseHash):
+        raise TypeError("hashalgo must be an object implementing BaseHash.")
 
-    if isinstance(hashalgo, str):
-        hash_ = Hash.new(hashalgo)
-    else:
-        # use our hashalgo
-        hash_ = hashalgo.new()
+    hash_ = hashalgo.new()
 
     key = KDF.HKDF(
         master=master_key,
         key_len=dklen,
         salt=salt,
-        hashmod=hash_,
+        hashmod=hash_,  # type: ignore
         num_keys=1,
         context=cipher_ctx,
     )
@@ -56,7 +49,7 @@ def derive_hkdf_key(
         master=master_key,
         key_len=hash_.digest_size,
         salt=salt,
-        hashmod=hash_,
+        hashmod=hash_,  # type: ignore
         num_keys=1,
         context=auth_ctx,
     )
--- pyflocker/ciphers/backends/cryptography_/misc.py
+++ pyflocker/ciphers/backends/cryptography_/misc.py
@@ -4,20 +4,18 @@ Miscellaneous Tools: Tools that are not common to symmetric or asymmetric.
 
 from __future__ import annotations
 
-from cryptography.hazmat.backends import default_backend as defb
 from cryptography.hazmat.primitives.ciphers import Cipher
 from cryptography.hazmat.primitives.ciphers import algorithms as algo
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 
 from ...base import BaseHash
-from .Hash import HASHES as _hashes
 from .Hash import _get_hash_algorithm
 
 
 def derive_hkdf_key(
     master_key: bytes,
     dklen: int,
-    hashalgo: str | BaseHash,
+    hashalgo: BaseHash,
     salt: bytes,
     cipher_ctx: bytes = b"enc-key",
     auth_ctx: bytes = b"auth-key",
@@ -25,41 +23,22 @@ def derive_hkdf_key(
     """Derive key materials for HMAC from given master key.
 
     Args:
-        master_key (bytes): The key used to derive the keys from.
-        dklen (int): Desired lenth of the derived key.
-        hashalgo (str, BaseHash): The name of the hash algorithm.
-        salt (bytes): The salt to use.
-        cipher_ctx (bytes): Context for cipher.
-        auth_ctx (bytes): Context for HMAC.
+        master_key: The key used to derive the keys from.
+        dklen: Desired lenth of the derived key.
+        hashalgo: The name of the hash algorithm.
+        salt: The salt to use.
+        cipher_ctx: Context for cipher.
+        auth_ctx: Context for HMAC.
 
     Returns:
         A pair of *cipher key* and *MAC key*.
     """
-    if not isinstance(hashalgo, (str, BaseHash)):
-        raise TypeError(
-            "hashalgo must be a str or an object implementing BaseHash"
-        )
+    if not isinstance(hashalgo, BaseHash):
+        raise TypeError("hashalgo must be an object implementing BaseHash")
 
-    if isinstance(hashalgo, str):
-        hash_ = _hashes[hashalgo]()
-    else:
-        hash_ = _get_hash_algorithm(hashalgo)
-
-    key = HKDF(
-        hash_,
-        dklen,
-        salt,
-        cipher_ctx,
-        defb(),
-    ).derive(master_key)
-
-    hkey = HKDF(
-        hash_,
-        hash_.digest_size,
-        salt,
-        auth_ctx,
-        defb(),
-    ).derive(master_key)
+    hash_ = _get_hash_algorithm(hashalgo)
+    key = HKDF(hash_, dklen, salt, cipher_ctx).derive(master_key)
+    hkey = HKDF(hash_, hash_.digest_size, salt, auth_ctx).derive(master_key)
     return key, hkey
 
 
