783c2be9a801bcdd34de21dc363a89236eb26d88
Arunanshu Biswas <mydellpc07@gmail.com>
2022-06-14 03:05:13 +0530

    test(test_ChaCha20): improve ChaCha20 tests
    Note: generic errors haven't been tested yet.

--- tests/test_ChaCha20.py
+++ tests/test_ChaCha20.py
@@ -1,86 +1,413 @@
 from __future__ import annotations
 
-import hashlib
-from functools import partial
-from itertools import product
+import io
 
 import pytest
+from hypothesis import given, settings
+from hypothesis import strategies as st
 
-from pyflocker.ciphers import ChaCha20
-from pyflocker.ciphers.backends import Backends
+from pyflocker.ciphers import Backends, base, exc
+from pyflocker.ciphers.backends.symmetric import FileCipherWrapper
+from pyflocker.ciphers.interfaces import ChaCha20
 
-from .base import BaseSymmetric, BaseSymmetricAEAD
+from .base import get_io_buffer, make_buffer
 
-TEST_VECTOR_KEY = hashlib.sha3_512(b"TEST_VECTOR_KEY for ChaCha20").digest()[
-    :32
-]
-TEST_VECTOR_NONCE = hashlib.sha3_512(
-    b"TEST_VECTOR_NONCE for ChaCha20"
-).digest()
+CHACHA20_BLOCK_SIZE = 1
+
+CHACHA20_KEY_SIZES = st.binary(min_size=32, max_size=32)
+
+CHACHA20_NONCE_SIZES = st.sampled_from((8, 12)).flatmap(
+    lambda size: st.binary(min_size=size, max_size=size)
+)
 
 
-@pytest.fixture
-def cipher(nonce_length, use_poly1305):
-    return partial(
-        ChaCha20.new,
-        key=TEST_VECTOR_KEY,
-        nonce=TEST_VECTOR_NONCE[:nonce_length],
-        use_poly1305=use_poly1305,
+def get_encryptor(
+    key: bytes,
+    nonce: bytes,
+    backend: Backends,
+    *,
+    use_poly1305: bool = False,
+    file: io.BufferedReader | None = None,
+) -> base.BaseAEADCipher | base.BaseNonAEADCipher | FileCipherWrapper:
+    try:
+        enc = ChaCha20.new(
+            True,
+            key,
+            nonce,
+            backend=backend,
+            file=file,
+            use_poly1305=use_poly1305,
+        )
+    except exc.UnsupportedAlgorithm:
+        return pytest.skip(f"Camellia not supported by {backend.name.lower()}")
+
+    assert isinstance(
+        enc,
+        (base.BaseAEADCipher, base.BaseNonAEADCipher, FileCipherWrapper),
+    )
+    return enc
+
+
+def get_decryptor(
+    key: bytes,
+    nonce: bytes,
+    backend: Backends,
+    *,
+    use_poly1305: bool = False,
+    file: io.BufferedReader | None = None,
+) -> base.BaseAEADCipher | base.BaseNonAEADCipher | FileCipherWrapper:
+    try:
+        dec = ChaCha20.new(
+            False,
+            key,
+            nonce,
+            backend=backend,
+            file=file,
+            use_poly1305=use_poly1305,
+        )
+    except exc.UnsupportedAlgorithm:
+        return pytest.skip(f"Camellia not supported by {backend.name.lower()}")
+
+    assert isinstance(
+        dec,
+        (
+            base.BaseAEADCipher,
+            base.BaseNonAEADCipher,
+            FileCipherWrapper,
+        ),
+    )
+    return dec
+
+
+def get_encryptor_decryptor(
+    key: bytes,
+    nonce: bytes,
+    backend1: Backends,
+    backend2: Backends,
+    *,
+    use_poly1305: bool = False,
+    plain_file: io.BufferedReader | None = None,
+    encrypted_file: io.BufferedReader | None = None,
+):
+    """Return a pair of encryptor and decryptor."""
+    return (
+        get_encryptor(
+            key,
+            nonce,
+            backend1,
+            use_poly1305=use_poly1305,
+            file=plain_file,
+        ),
+        get_decryptor(
+            key,
+            nonce,
+            backend2,
+            use_poly1305=use_poly1305,
+            file=encrypted_file,
+        ),
     )
 
 
-@pytest.mark.parametrize(
-    "backend1, backend2",
-    list(product(Backends, repeat=2)),
-)
-@pytest.mark.parametrize(
-    "nonce_length",
-    [8, 12],
-)
-@pytest.mark.parametrize(
-    "use_poly1305",
-    [False],
-)
-class TestChaCha20(BaseSymmetric):
-    def test_update_into(self, cipher, backend1, backend2):
-        return super().test_update_into(
-            cipher,
+class TestChaCha20:
+    @settings(deadline=None)
+    @pytest.mark.parametrize("backend1", Backends)
+    @pytest.mark.parametrize("backend2", Backends)
+    @given(
+        key=CHACHA20_KEY_SIZES,
+        nonce=CHACHA20_NONCE_SIZES,
+        data=st.binary(),
+    )
+    def test_update(
+        self,
+        key: bytes,
+        nonce: bytes,
+        data: bytes,
+        backend1: Backends,
+        backend2: Backends,
+    ):
+        encryptor, decryptor = get_encryptor_decryptor(
+            key,
+            nonce,
             backend1,
             backend2,
-            offset=0,
         )
+        assert not isinstance(encryptor, FileCipherWrapper)
+        assert not isinstance(decryptor, FileCipherWrapper)
 
+        assert decryptor.update(encryptor.update(data)) == data
 
-@pytest.mark.parametrize(
-    "backend1, backend2",
-    list(product(Backends, repeat=2)),
-)
-@pytest.mark.parametrize(
-    "nonce_length",
-    [8, 12],
-)
-@pytest.mark.parametrize(
-    "use_poly1305",
-    [True],
-)
-class TestChaCha20Poly1305(BaseSymmetricAEAD):
-    def test_update_into(self, cipher, backend1, backend2):
-        return super().test_update_into(
-            cipher,
+    @settings(deadline=None)
+    @pytest.mark.parametrize("backend1", Backends)
+    @pytest.mark.parametrize("backend2", Backends)
+    @given(
+        key=CHACHA20_KEY_SIZES,
+        nonce=CHACHA20_NONCE_SIZES,
+        data=st.binary(),
+    )
+    def test_update_into(
+        self,
+        key: bytes,
+        nonce: bytes,
+        data: bytes,
+        backend1: Backends,
+        backend2: Backends,
+    ):
+        encryptor, decryptor = get_encryptor_decryptor(
+            key,
+            nonce,
             backend1,
             backend2,
-            offset=0,
         )
+        assert not isinstance(encryptor, FileCipherWrapper)
+        assert not isinstance(decryptor, FileCipherWrapper)
 
+        buffer = make_buffer(data, CHACHA20_BLOCK_SIZE - 1)
+
+        in_, out = get_io_buffer(buffer, backend1, CHACHA20_BLOCK_SIZE - 1)
+        encryptor.update_into(in_, out)
+
+        in_, out = get_io_buffer(buffer, backend2, CHACHA20_BLOCK_SIZE - 1)
+        decryptor.update_into(in_, out)
+
+        assert data == buffer[: len(data)].tobytes()
+
+    @settings(deadline=None)
+    @pytest.mark.parametrize("backend1", Backends)
+    @pytest.mark.parametrize("backend2", Backends)
+    @given(
+        key=CHACHA20_KEY_SIZES,
+        nonce=CHACHA20_NONCE_SIZES,
+        data=st.binary(),
+        authdata=st.binary(min_size=1),
+    )
+    def test_update_with_auth(
+        self,
+        key: bytes,
+        nonce: bytes,
+        data: bytes,
+        authdata: bytes,
+        backend1: Backends,
+        backend2: Backends,
+    ):
+        encryptor, decryptor = get_encryptor_decryptor(
+            key,
+            nonce,
+            backend1,
+            backend2,
+            use_poly1305=True,
+        )
+        assert isinstance(encryptor, base.BaseAEADCipher)
+        assert isinstance(decryptor, base.BaseAEADCipher)
+
+        encryptor.authenticate(authdata)
+        encrypted = encryptor.update(data)
+
+        decryptor.authenticate(authdata)
+        decrypted = decryptor.update(encrypted)
+
+        encryptor.finalize()
+        decryptor.finalize(encryptor.calculate_tag())
+
+        assert data == decrypted
+
+    @settings(deadline=None)
+    @pytest.mark.parametrize("backend1", Backends)
+    @pytest.mark.parametrize("backend2", Backends)
+    @given(
+        key=CHACHA20_KEY_SIZES,
+        nonce=CHACHA20_NONCE_SIZES,
+        data=st.binary(),
+        authdata=st.binary(min_size=1),
+    )
     def test_update_into_with_auth(
         self,
-        cipher,
-        backend1,
-        backend2,
+        key: bytes,
+        nonce: bytes,
+        data: bytes,
+        authdata: bytes,
+        backend1: Backends,
+        backend2: Backends,
     ):
-        return super().test_update_into_with_auth(
-            cipher,
+        encryptor, decryptor = get_encryptor_decryptor(
+            key,
+            nonce,
             backend1,
             backend2,
-            offset=0,
+            use_poly1305=True,
         )
+        assert isinstance(encryptor, base.BaseAEADCipher)
+        assert isinstance(decryptor, base.BaseAEADCipher)
+        buffer = make_buffer(data, CHACHA20_BLOCK_SIZE - 1)
+
+        in_, out = get_io_buffer(buffer, backend1, CHACHA20_BLOCK_SIZE - 1)
+        encryptor.authenticate(authdata)
+        encryptor.update_into(in_, out)
+
+        in_, out = get_io_buffer(buffer, backend2, CHACHA20_BLOCK_SIZE - 1)
+        decryptor.authenticate(authdata)
+        decryptor.update_into(in_, out)
+
+        encryptor.finalize()
+        decryptor.finalize(encryptor.calculate_tag())
+
+        assert data == buffer[: len(data)].tobytes()
+
+    @settings(deadline=None)
+    @pytest.mark.parametrize("backend", Backends)
+    @given(
+        key=st.binary().filter(lambda b: len(b) != 32),
+        nonce=CHACHA20_NONCE_SIZES,
+    )
+    def test_invalid_key_length(
+        self,
+        key,
+        nonce,
+        backend,
+    ):
+        with pytest.raises(ValueError):
+            get_encryptor(key, nonce, backend)
+
+    @settings(deadline=None)
+    @pytest.mark.parametrize("backend", Backends)
+    @given(
+        key=CHACHA20_KEY_SIZES,
+        # NOTE: Cryptodome supports both ChaCha20 and XChaCha20, hence the 24
+        # bytes nonce. For cryptography, it should be invalid too.
+        nonce=st.binary().filter(lambda b: len(b) not in (8, 12, 24)),
+    )
+    def test_invalid_nonce_length(
+        self,
+        key,
+        nonce,
+        backend,
+    ):
+        with pytest.raises(ValueError):
+            get_encryptor(key, nonce, backend)
+
+
+class TestFileIO:
+    @settings(deadline=None)
+    @pytest.mark.parametrize("backend1", Backends)
+    @pytest.mark.parametrize("backend2", Backends)
+    @given(
+        key=CHACHA20_KEY_SIZES,
+        nonce=CHACHA20_NONCE_SIZES,
+        data=st.binary(min_size=1),
+        authdata=st.none() | st.binary(min_size=1),
+    )
+    def test_update_into(
+        self,
+        key: bytes,
+        nonce: bytes,
+        data: bytes,
+        authdata: bytes,
+        backend1: Backends,
+        backend2: Backends,
+    ):
+        filebuf = io.BytesIO(data)
+        as_encrypted = io.BytesIO()
+        as_decrypted = io.BytesIO()
+
+        encryptor, decryptor = get_encryptor_decryptor(
+            key,
+            nonce,
+            backend1,
+            backend2,
+            plain_file=filebuf,
+            encrypted_file=as_encrypted,
+        )
+        assert isinstance(encryptor, FileCipherWrapper)
+        assert isinstance(decryptor, FileCipherWrapper)
+
+        if authdata is not None:
+            encryptor.authenticate(authdata)
+            decryptor.authenticate(authdata)
+
+        encryptor.update_into(as_encrypted)
+        as_encrypted.seek(0)
+        decryptor.update_into(as_decrypted, encryptor.calculate_tag())
+
+        assert filebuf.getvalue() == as_decrypted.getvalue()
+
+    @settings(deadline=None)
+    @pytest.mark.parametrize("backend1", Backends)
+    @pytest.mark.parametrize("backend2", Backends)
+    @given(
+        key=CHACHA20_KEY_SIZES,
+        nonce=CHACHA20_NONCE_SIZES,
+        data=st.binary(min_size=1),
+        authdata=st.none() | st.binary(min_size=1),
+    )
+    def test_update(
+        self,
+        key: bytes,
+        nonce: bytes,
+        data: bytes,
+        authdata: bytes,
+        backend1: Backends,
+        backend2: Backends,
+    ):
+        filebuf = io.BytesIO(data)
+        as_encrypted = io.BytesIO()
+
+        encryptor, decryptor = get_encryptor_decryptor(
+            key,
+            nonce,
+            backend1,
+            backend2,
+            plain_file=filebuf,  # type: ignore
+            encrypted_file=as_encrypted,  # type: ignore
+        )
+        assert isinstance(encryptor, FileCipherWrapper)
+        assert isinstance(decryptor, FileCipherWrapper)
+
+        if authdata is not None:
+            encryptor.authenticate(authdata)
+            decryptor.authenticate(authdata)
+
+        as_encrypted.write(encryptor.update(len(data)))  # type: ignore
+        as_encrypted.seek(0)
+
+        as_decrypted = decryptor.update(len(data))
+        assert as_decrypted == data
+
+    @settings(deadline=None)
+    @pytest.mark.parametrize("backend1", Backends)
+    @pytest.mark.parametrize("backend2", Backends)
+    @given(
+        key=CHACHA20_KEY_SIZES,
+        nonce=CHACHA20_NONCE_SIZES,
+        data=st.binary(min_size=1),
+        authdata=st.binary(min_size=1),
+    )
+    def test_fileio_incorrect_decryption(
+        self,
+        key: bytes,
+        nonce: bytes,
+        data: bytes,
+        authdata: bytes,
+        backend1: Backends,
+        backend2: Backends,
+    ):
+        filebuf = io.BytesIO(data)
+        as_encrypted = io.BytesIO()
+        as_decrypted = io.BytesIO()
+
+        encryptor, decryptor = get_encryptor_decryptor(
+            key,
+            nonce,
+            backend1,
+            backend2,
+            plain_file=filebuf,
+            encrypted_file=as_encrypted,
+        )
+        assert isinstance(encryptor, FileCipherWrapper)
+        assert isinstance(decryptor, FileCipherWrapper)
+
+        encryptor.authenticate(authdata)
+
+        encryptor.update_into(as_encrypted)
+        as_encrypted.seek(0)
+
+        with pytest.raises(exc.DecryptionError):
+            decryptor.update_into(as_decrypted, encryptor.calculate_tag())
